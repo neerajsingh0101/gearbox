81a859b067aeda515305d8d873839a0b042174da
Neeraj Singh <neerajdotname@gmail.com>
2015-01-30 11:43:31 -0500

    first commit

---
+++ config/application.rb
@@ -0,0 +1,28 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Wheel
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+
+    # The default value is true. This option is here so that users running tests from within vim
+    # can turn off colorized logging easily. Still looking for an easy solution to accomplish it.
+    # config.colorize_logging = false
+
+  end
+end
